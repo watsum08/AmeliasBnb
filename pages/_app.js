eca6abcf585f69646fa08057e4a34b1c611a8995
marcmeynet <info@marcmeynet.ch>
2022-07-19 18:37:30 +0200

    added chakraui

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { ChakraProvider, Container } from "@chakra-ui/react";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider>
+      <Container maxW={1200} bg="yellow.400" p={0}>
+        <Component {...pageProps} />
+      </Container>
+    </ChakraProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
