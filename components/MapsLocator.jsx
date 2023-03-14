9b5c2a01fc5473ba3eb2e5c8483e519e4ef8a05d
Marc Meynet <marcmeynet@gmail.com>
2023-03-14 05:35:41 +0100

    (feat) added google maps

--- components/MapsLocator.jsx
+++ components/MapsLocator.jsx
@@ -1,8 +1,14 @@
-import { Box } from "@chakra-ui/react";
-
 const MapsLocator = () => {
   return (
-    <Box></Box>
-  )
+    <iframe
+      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d265.2952313192532!2d6.860119196030422!3d46.382028992588666!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x478e9953499c32c1%3A0xae844844602422f7!2sRoute%20des%20Iles%204%2C%201897%20Port-Valais!5e0!3m2!1sfr!2sch!4v1587330765434!5m2!1sfr!2sch"
+      width="100%"
+      height="100%"
+      frameBorder="0"
+      allowFullScreen=""
+      aria-hidden="false"
+      tabIndex="0"
+    ></iframe>
+  );
 };
 export default MapsLocator;
